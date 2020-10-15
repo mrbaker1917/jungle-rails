09fb954b1a6fc869f5185de8b022b9c084e8a2fb
Mark Baker <mrbaker1917@gmail.com>
2020-10-15 07:46:47 -0700

    implements About Us link and page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resource :about, only: [:show]
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
