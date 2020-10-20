f09e63564598e69b94627cb2218ce5de3b87aacd
Mark Baker <mrbaker1917@gmail.com>
2020-10-20 16:11:07 -0700

    adds test to check that cart add works

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,27 +1,28 @@
 require 'rails_helper'
 
 RSpec.feature "AddProducts", type: :feature, js: true do
-  
+  # SETUP
   before :each do
-    # setup
     @category = Category.create! name: 'Apparel'
-    @category.products.create!(
-      name: 'Women\'s Zebra pants',
-      description: Faker::Hipster.paragraph(4),
-      image: '',
-      price: 50.50,
-      quantity: 5
-    )
+  
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
   end
+
 
   scenario "A user can add a product to their cart when not logged in" do
     # visit the home page
     visit '/'
     # find a product listing
     first('article.product').find_button('Add').click
-    find_link('My Cart (1)').click
-    expect(page).to have_content('TOTAL:')
-    expect(page).to have_content('Women\'s Zebra pants')
+    expect(page).to have_content('My Cart (1)')
     page.save_screenshot
   end
 end
