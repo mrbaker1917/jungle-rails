efa87ab0950f88c4516839da5159aca8618732bc
Mark Baker <mrbaker1917@gmail.com>
2020-10-20 17:01:38 -0700

    adds capybara test for login

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "UserLogins", type: :feature, js: true do
+  before(:each) do
+    @a_user = User.create!(first_name: "Mark", last_name: "Baker", email: 'mrbaker1917@gmail.com', password: "B1p2m3td$", password_confirmation: "B1p2m3td$")
+  end
+
+  scenario "user can log in with correct email and password" do
+    # visit home page
+    visit root_path
+    # click on login
+    click_on "Login"
+    expect(page).to have_content('Email')
+    fill_in 'email', with: @a_user.email
+    fill_in 'password', with: @a_user.password
+    click_button 'Submit'
+    expect(page).to have_content('Logout')
+  end
+end
